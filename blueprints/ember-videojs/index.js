9e6b7e67f843e3a0a3003ce09bae190b036fa37d
Snow <snow@hollysydney.com>
2018-10-30 08:17:57 +1100

    Add dependency. Remove shim.

---
+++ blueprints/ember-videojs/index.js
@@ -0,0 +1,8 @@
+// blueprints/ember-videojs/index.js
+module.exports = {
+  normalizeEntityName() {}, // no-op since we're just adding dependencies
+
+  afterInstall() {
+    return this.addPackageToProject('video.js');
+  }
+};
