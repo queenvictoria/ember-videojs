a625f15216d77fde429c4530cb440b116115faeb
Snow <snow@hollysydney.com>
2018-10-30 08:29:21 +1100

    Trying all the things.

--- blueprints/ember-videojs/index.js
+++ blueprints/ember-videojs/index.js
@@ -3,6 +3,6 @@ module.exports = {
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
   afterInstall() {
-    return this.addPackageToProject('video.js');
+    return this.addPackageToProject('videojs', 'video.js');
   }
 };
