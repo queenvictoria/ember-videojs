c7e28b667b30726b7739612fd3390cfacc4e5a46
Snow <snow@hollysydney.com>
2018-10-30 08:33:12 +1100

    Come on pay attention.

--- addon/components/videojs-base.js
+++ addon/components/videojs-base.js
@@ -1,6 +1,6 @@
 import Component from '@ember/component';
 import layout from '../templates/components/videojs-base';
-import videojs from 'video.js';
+import videojs from 'videojs';
 
 export default Component.extend({
   layout,
