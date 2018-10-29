4181b2bdc25eb76cf31addd771e0112d10b1d8e9
Snow <snow@hollysydney.com>
2018-10-30 08:16:52 +1100

    Reduce warnings.

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,8 +1,13 @@
-import Ember from "ember";
+import Controller from '@ember/controller';
 
-export default Ember.Controller.extend({
-  sources: [
-    { src: "https://vjs.zencdn.net/v/oceans.mp4", type: "video/mp4" },
-    { src: "https://vjs.zencdn.net/v/oceans.webm", type: "video/webm" }
-  ],
+export default Controller.extend({
+
+  init() {
+    this._super(...arguments);
+    this.sources = [
+      { src: "https://vjs.zencdn.net/v/oceans.mp4", type: "video/mp4" },
+      { src: "https://vjs.zencdn.net/v/oceans.webm", type: "video/webm" }
+    ];
+  },
+
 });
