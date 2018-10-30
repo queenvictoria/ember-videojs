9297458aae719b6018a37b4dc51ed75d1a9e47f6
Snow <snow@hollysydney.com>
2018-10-30 13:38:37 +1100

    Match shim style.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-env node */
 'use strict';
 const Funnel = require('broccoli-funnel');
 const MergeTrees = require('broccoli-merge-trees');
@@ -10,21 +11,21 @@ module.exports = {
   included(app) {
     this._super.included.apply(this, arguments);
 
-    const videojsDirectory = 'node_modules/video.js/dist';
+    const baseDirectory = 'node_modules/video.js/dist';
 
     app.import({
-      development: path.join(videojsDirectory, 'video-js.css'),
-      production:  path.join(videojsDirectory, 'video-js.min.css')
+      development: path.join(baseDirectory, 'video-js.css'),
+      production:  path.join(baseDirectory, 'video-js.min.css')
     });
 
-    // app.import(path.join(videojsDirectory, 'font/VideoJS.eot'), { destDir: 'assets/font' });
-    // app.import(path.join(videojsDirectory, 'font/VideoJS.svg'), { destDir: 'assets/font' });
-    // app.import(path.join(videojsDirectory, 'font/VideoJS.ttf'), { destDir: 'assets/font' });
-    // app.import(path.join(videojsDirectory, 'font/VideoJS.woff'), { destDir: 'assets/font' });
+    // app.import(path.join(baseDirectory, 'font/VideoJS.eot'), { destDir: 'assets/font' });
+    // app.import(path.join(baseDirectory, 'font/VideoJS.svg'), { destDir: 'assets/font' });
+    // app.import(path.join(baseDirectory, 'font/VideoJS.ttf'), { destDir: 'assets/font' });
+    // app.import(path.join(baseDirectory, 'font/VideoJS.woff'), { destDir: 'assets/font' });
 
     app.import({
-      development: path.join(videojsDirectory, 'video.js'),
-      production: path.join(videojsDirectory, 'video.min.js'),
+      development: path.join(baseDirectory, 'video.js'),
+      production: path.join(baseDirectory, 'video.min.js'),
     });
     app.import('vendor/shims/videojs.js');
   },
