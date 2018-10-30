da2a7f23c730cb9b748d436b5bb37389f2397502
Queenvictoria <snow@hollysydney.com>
2018-10-30 13:33:10 +1100

    Detect and warn.

--- addon/components/videojs-base.js
+++ addon/components/videojs-base.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 import layout from '../templates/components/videojs-base';
 import videojs from 'videojs';
+// import videojsvr from 'videojs-vr';
 
 export default Component.extend({
   layout,
@@ -20,8 +21,12 @@ export default Component.extend({
     // Register plugins
     // Get global plugins from config.
     if ( this.get("vr-projection") ) {
-      this.set("vr-crossorigin", "anonymous");
-      player.vr({projection: this.get("vr-projection")});
+      if ( typeof player.vr === "function" ) {
+        this.set("vr-crossorigin", "anonymous");
+        player.vr({projection: this.get("vr-projection")});
+      } else {
+        console.error("It looks like you are trying to play a VR video without the videojs-vr library. Please ember install ember-videojs-vr-shim.");
+      }
     }
 
     this.set("player", player);
